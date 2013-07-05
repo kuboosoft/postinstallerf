6487e793f8003f087997e125988dbbb62dc92d9b
Max M <maxmrphy@gmail.com>
2013-07-05 16:57:43 -0700

    Translated all comments in gnextnav.py

--- gnextnav.py
+++ gnextnav.py
@@ -14,36 +14,36 @@ class SimpleBrowser: # needs GTK, Python, Webkit-GTK (pywebkitgtk)
 		self.txt_url = gtk.Entry()
 		self.txt_url.connect('activate', self._txt_url_activate)
 
-		# Genero una barra de herramientas
+		# Generating a toolbar.
 		toolbar = gtk.HBox(spacing=5)
 		vbox.pack_start(toolbar,False,False)
 
-		# Agrego el boton "Atras"
+		# I add the button "Back"
 		self.btnback = gtk.Button()
 		self.btnback.add(gtk.Arrow(gtk.ARROW_LEFT,gtk.SHADOW_OUT))
 		self.btnback.connect('clicked',self._go_back)
 		toolbar.pack_start(self.btnback,False,False)
 
-		# Agrego el boton "Adelante"
+		# I add the button "Forward"
 		self.btnforward = gtk.Button()
 		self.btnforward.add(gtk.Arrow(gtk.ARROW_RIGHT,gtk.SHADOW_OUT))
 		self.btnforward.connect('clicked',self._go_forward)
 		toolbar.pack_start(self.btnforward,False,False)
 
-		# Agrego el boton "Actualizar"
+		# I add the "Refresh"
 		btnrefresh = gtk.Button('Actualizar')
 		btnrefresh.connect('clicked',self._refresh)
 		toolbar.pack_start(btnrefresh,False,False)
 
 		
-		# Agrego el renderer del motor
+		# I add the renderer engine
 		self.scrolled_window = gtk.ScrolledWindow()
 		self.webview = webkit.WebView()
 		self.scrolled_window.add(self.webview)
 		vbox.pack_start(self.scrolled_window, fill=True, expand=True)
 		self.window.add(vbox)
 
-		# Agrego un alinea de estado, con una etiqueta y una barra de progreso
+		# Adding a status line, with a label and a progress bar
 		self.pbar = gtk.ProgressBar()
 		self.status = gtk.Label()
 		hbox2 = gtk.HBox(False,0)
@@ -51,7 +51,7 @@ class SimpleBrowser: # needs GTK, Python, Webkit-GTK (pywebkitgtk)
 		hbox2.pack_end(self.pbar,False,False)
 		vbox.pack_start(hbox2,False,True)
 
-		# Defino las acciones a realizar segun los eventos del motor html
+		# Define the actions to take second html engine events
 		self.webview.connect('load-started',self._load_start)
 		self.webview.connect('load-progress-changed',self._load_progress_changed)
 		self.webview.connect('load-finished',self._load_finished)
@@ -65,7 +65,7 @@ class SimpleBrowser: # needs GTK, Python, Webkit-GTK (pywebkitgtk)
 	def _load(self, url):
 		self.webview.open(url)
 	def open(self, url):
-		# Si la url no tiene el https:// adelante, se lo agrego
+		# If the url does not have the https:// I added it later.
 		if url[0:7] != "https://":
 			url = "https://"+url
 		self.txt_url.set_text(url)
@@ -89,13 +89,13 @@ class SimpleBrowser: # needs GTK, Python, Webkit-GTK (pywebkitgtk)
 	def _refresh(self,nada):
 		self.webview.reload()
 	def _title_changed(self,view,frame,title):
-		# Actualizo el titulo del navegador, la url en la barra de url y activo/desactivo los botones Adelante y Atras
+		# Refresh the browser title, the url in url bar and enable / disable the Back and Forward buttons
 		self.window.set_title('%s' % title)
 		self.text.set_text(frame.get_uri())
 		self.btnback.props.sensitive = self.webview.can_go_back()
 		self.btnforward.props.sensitive = self.webview.can_go_forward()
 	def _hover_link(self,view,frame,url):
-		# Si se hace hover sobre un link, pongo en la barra de estado la url hacia la que linkea
+		# If hovering over a link, put in the status bar to the url to the link
 		if view and url:
 			self.status.set_text(url)
 		else:
